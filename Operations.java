1f1689be2e767609523d4adc482de77d59486329
khadar-29 <khadarvalimb@gmail.com>
2025-04-24 16:24:06 +0530

    save

---
+++ Operations.java
@@ -0,0 +1,10 @@
+public class Operations{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int sub = a - b;
+        System.out.println("sum is=" + sum);
+        System.out.println("sub is=" + sub);
+    }
+}
